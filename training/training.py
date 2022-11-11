686bdf890fe91857cb6d7256879aeb9b80b144d0
Hiroshi Urayama <urahiroshi@mercari.com>
2022-11-11 10:15:20 +0900

    execute evaluation by test table

--- training/training.py
+++ training/training.py
@@ -12,21 +12,26 @@ def separate_plus_and_minus(original):
     result.append(0 if v >= 0 else -v)
   return result
 
-with open('./get-training-table/table.yaml') as file:
-  table = yaml.safe_load(file)
-  training_good = np.array(
-    [separate_plus_and_minus(row[0]) for row in table]
-  ) / 2000.0
-  training_bad = np.array(
-    [separate_plus_and_minus(row[1]) for row in table]
-  ) / 2000.0
-  training_good_bad = np.concatenate([training_good, training_bad])
-  training_bad_good = np.concatenate([training_bad, training_good])
-  training_labels = np.concatenate([
-    np.array([[1.0, 0.0] for _ in table]),
-    np.array([[0.0, 1.0] for _ in table]),
-  ])
+def get_formatted_data(file_path):
+  with open(file_path) as file:
+    table = yaml.safe_load(file)
+    good = np.array(
+      [separate_plus_and_minus(row[0]) for row in table]
+    ) / 2000.0
+    bad = np.array(
+      [separate_plus_and_minus(row[1]) for row in table]
+    ) / 2000.0
+    good_bad = np.concatenate([good, bad])
+    bad_good = np.concatenate([bad, good])
+    labels = np.concatenate([
+      np.array([[1.0, 0.0] for _ in table]),
+      np.array([[0.0, 1.0] for _ in table]),
+    ])
+    return [good_bad, bad_good, labels]
 
+training_good_bad, training_bad_good, training_labels = get_formatted_data(
+  './get-training-table/table.yaml'
+)
 input_good_bad = tf.keras.layers.Input(shape=(8))
 input_bad_good = tf.keras.layers.Input(shape=(8))
 shared_dence_1 = tf.keras.layers.Dense(4, activation=tf.nn.relu)
@@ -52,3 +57,8 @@ model.compile(
 )
 
 model.fit([training_good_bad, training_bad_good], training_labels, epochs=300)
+
+test_good_bad, test_bad_good, test_labels = get_formatted_data(
+  './get-test-table/test-table.yaml'
+)
+model.evaluate([test_good_bad, test_bad_good], test_labels)
