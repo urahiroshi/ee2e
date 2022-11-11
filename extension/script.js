949acf6b68be817594e0dcffe5b33de5425b5d93
Hiroshi Urayama <urahiroshi@mercari.com>
2022-11-11 18:38:04 +0900

    fix file references

--- extension/script.js
+++ extension/script.js
@@ -1,5 +1,5 @@
 const execE2E = (command, target) => {
-  const src = chrome.runtime.getURL('content-script/main.js');
+  const src = chrome.runtime.getURL('content-script/module.js');
   import(src).then(({ main }) => {
     main({ command, target });
   });
